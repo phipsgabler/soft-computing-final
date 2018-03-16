d9703642067b130b8ce4dd0375576964e2b3d5a8
Philipp Gabler <phipsgabler@users.noreply.github.com>
2018-03-16 09:58:03 +0200

    Restructure files, add epsilon macro

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,7 @@ using SoftComputingFinal
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+t = Branch{2}([(Variable{2}(1), 0.1), (Variable{2}(2), 3)],
+              1.0,
+              Variable{2}(2),
+              Variable{2}(1))
